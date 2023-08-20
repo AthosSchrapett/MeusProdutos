dfc0b303c21fcb1be95665c76bc3a502c706a20f
Athos Schrapett <athosschrapett@outlook.com>
2023-08-20 12:41:43 -0300

    fix: inclusão de Data no projeto

---
+++ MeusProdutos.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using MeusProdutos.Models.Interfaces.Repositories;
+using MeusProdutos.Repositories.Context;
+
+namespace MeusProdutos.Repositories.Repositories;
+
+public class BaseRepository<T> : IBaseRepository<T> where T : class
+{
+    protected readonly MeusProdutosContext? _context;
+
+    public BaseRepository(MeusProdutosContext? context) => _context = context;
+
+    public void Add(T entity) => _context?.Set<T>().Add(entity);
+    public void Delete(T entity) => _context?.Set<T>().Remove(entity);
+    public IEnumerable<T>? GetAll() => _context?.Set<T>();
+    public T? GetById(int id) => _context?.Set<T>().Find(id);
+    public void Update(T entity) => _context?.Update(entity);
+}
---
+++ MeusProdutos.Data/Repositories/ProdutoRepository.cs
@@ -0,0 +1,12 @@
+﻿using MeusProdutos.Models.Entities;
+using MeusProdutos.Models.Interfaces.Repositories;
+using MeusProdutos.Repositories.Context;
+
+namespace MeusProdutos.Repositories.Repositories;
+
+public class ProdutoRepository : BaseRepository<Produto>, IProdutoRepository
+{
+    public ProdutoRepository(MeusProdutosContext? context) : base(context)
+    {
+    }
+}
